29b5a7fb9574d5df49d2e9eadf8ee9b08a8e5cc4
Stephanie Koltun <skoltun@payette.com>
2016-03-04 10:24:40 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset='utf-8'>
+    <title>PHPiggy Bank</title>
+
+    <?php
+
+    # Define 4 different variables, which will each represent how much a given coin is worth
+    $penny_value   = .01;
+    $nickle_value  = .05;
+    $dime_value    = .10;
+    $quarter_value = .25;
+
+    # Define 4 more variables, which will each represent how many of each coin is in the bank
+    $pennies  = 100;
+    $nickles  = 25;
+    $dimes    = 100;
+    $quarters = 34;
+
+    # Add up how much money is in the piggy bank
+    $total = ($pennies * $penny_value) + ($nickles * $nickle_value) + ($dimes * $dime_value) + ($quarters * $quarter_value);
+
+    ?>
+
+</head>
+
+<body>
+
+    <p>
+     You have $<?php echo $total; ?> in your piggy bank.
+    </p>
+
+</body>
+</html>
